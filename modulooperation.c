029e812071ac904888c9ba15b12faa6dbf844c3b
Akashchib <146857980+Akashchib@users.noreply.github.com>
2024-01-05 11:45:47 +0530

    Create modulooperation.c

---
+++ modulooperation.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+//main function heading
+void main()
+{
+//assignment of values
+int a=2,b=3,c=4,d,num=0;
+//input statement from user
+printf("Enter the number\n");
+scanf("%d",&num);
+d=num%a;
+//output statements
+printf("%d\n",d);
+d=num%b;
+printf("%d\n",d);
+d=num%c;
+printf("%d\n",d);
+}

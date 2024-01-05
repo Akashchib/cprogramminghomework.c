3452c69f25142cf2ca15c45632e793991b7f47a7
Akashchib <146857980+Akashchib@users.noreply.github.com>
2024-01-05 21:39:39 +0530

    Create newline.c

---
+++ newline.c
@@ -0,0 +1,9 @@
+//program to print Hello world using new line character
+//header files
+#include<stdio.h>
+//function heading
+void main()
+{
+//output statement
+printf("Hello world\nHello world");
+}

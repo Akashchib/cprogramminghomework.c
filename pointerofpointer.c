31063320e7b808a22d5506ff8aab6850658cd24c
Akashchib <146857980+Akashchib@users.noreply.github.com>
2024-01-05 20:59:53 +0530

    Create pointerofpointer.c

---
+++ pointerofpointer.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//main function heading
+void main()
+{
+//assignment of value(variable name)
+int i=10;
+//taking the address of i and store in P
+int*P=&i;
+//taking the address of P and store in PP
+int**PP=&P;
+//output statements
+printf("I=%d\n",i);
+printf("I=%d\n",*P);
+printf("I=%d\n",**PP);
+}

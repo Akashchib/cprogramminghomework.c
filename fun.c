8c6a818a3c6ea74103418da561f74252d51bfd21
Akashchib <146857980+Akashchib@users.noreply.github.com>
2024-01-05 21:05:26 +0530

    Create fun.c

---
+++ fun.c
@@ -0,0 +1,15 @@
+int add(int x ,int y )
+{
+//statement for addition
+int z=x+y;
+//return statement for addition
+return z;
+}
+//function taking for subtraction
+int sub(int x,int y)
+{
+//statement for subtraction
+int z=x-y;
+//return statement for subtraction
+return z;
+}

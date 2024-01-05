95e20bd24b9fbc0a4396e26476804b3c7c331981
Akashchib <146857980+Akashchib@users.noreply.github.com>
2024-01-05 21:00:37 +0530

    Create poin.c

---
+++ poin.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//main function heading
+void main()
+{
+// variable name declaration and assignment of value
+int i=10;
+//pointer declaration
+int *p;
+//taking the address of of i and store in P
+p=&i;
+//output statements
+printf("I=%d\n",i);
+printf("I=%d\n",*p);
+printf("I=%d\n",p);
+}

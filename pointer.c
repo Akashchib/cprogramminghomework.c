ecb2849b45b736f9a1b5d633b3df356428abcf62
Akashchib <146857980+Akashchib@users.noreply.github.com>
2024-01-05 20:59:04 +0530

    Create pointer.c

---
+++ pointer.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//main function heading
+void main()
+{
+//assignment of value(variable name)
+int i=10;
+//taking the address of i and storing in P
+int*P=&i;
+//increment statement
+P++;
+//output statements
+printf("I=%d\n",i);
+printf("I=%d\n",*P);
+printf("I=%d\n",P);
+}
